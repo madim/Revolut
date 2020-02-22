cc8bdf68696b221b76634d0c43a7b15ea559dcfa
madim <mmadikk@gmail.com>
2020-02-22 21:03:33 +0600

    Hide keyboard on scroll

--- app/src/main/java/com/example/revolut/presentation/MainActivity.kt
+++ app/src/main/java/com/example/revolut/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.revolut.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewConfiguration
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import com.example.revolut.R
 import com.example.revolut.presentation.adapter.RatesAdapter
+import com.example.revolut.presentation.util.onScroll
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
@@ -29,5 +34,22 @@ class MainActivity : AppCompatActivity() {
         ratesViewModel.message.observe(this, Observer {
             Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         })
+
+        val touchSlop = ViewConfiguration.get(this).scaledTouchSlop
+        var totalDy = 0
+        rateList.onScroll { _, dy ->
+            if (dy > 0) {
+                totalDy += dy
+                if (totalDy >= touchSlop) {
+                    totalDy = 0
+
+                    val inputMethodManager: InputMethodManager? = getSystemService()
+                    inputMethodManager?.hideSoftInputFromWindow(
+                        rateList.windowToken,
+                        HIDE_NOT_ALWAYS
+                    )
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/revolut/presentation/util/RecyclerViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.example.revolut.presentation.util
+
+import androidx.recyclerview.widget.RecyclerView
+
+internal inline fun RecyclerView.onScroll(crossinline body: (dx: Int, dy: Int) -> Unit) {
+    addOnScrollListener(object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            body(dx, dy)
+        }
+    })
+}
